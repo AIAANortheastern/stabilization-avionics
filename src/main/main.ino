1194c8a47d868b30f60962bb9941b58e3e041235
Adam Mirza <amirza495@gmail.com>
2018-01-30 20:56:24 -0500

    triggers solenoids on pins 2 and 3 at a rate of 0.5 Hz

---
+++ src/main/main.ino
@@ -0,0 +1,25 @@
+#define SOLENOID1 2
+#define SOLENOID2 3
+
+void setup() {
+  // put your setup code here, to run once:
+
+  pinMode(SOLENOID1, OUTPUT);
+  pinMode(SOLENOID2, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  digitalWrite(SOLENOID1, HIGH);
+  digitalWrite(SOLENOID2, HIGH);
+
+  delay(1000);
+
+  digitalWrite(SOLENOID1, LOW);
+  digitalWrite(SOLENOID2, LOW);
+
+  delay(1000);
+
+}
