80751331ebce9860edbeb115eb7da44f8e397c78
Adam Mirza <amirza495@gmail.com>
2018-06-25 20:59:40 -0400

    cleaned up code

--- src/main/main.ino
+++ src/main/main.ino
@@ -4,13 +4,12 @@
 #include "rt_nonfinite.h"
 #include "KalmanFilter_Initialize.h"
 
-extern void KalmanFilter_init(void);
-extern void KalmanFilter(double x[12], const double z[6]);
-
 sensor_data_t gSensorData;
 
 double rawSensorData[6];
 double state[12];
+double calibration[12];
+double output[12];
 
 void setup() {
 
@@ -30,22 +29,75 @@ void setup() {
   state[9] = 0;
   state[10] = 0;
   state[11] = 0;
+
+  calibration[0] = 0;
+  calibration[1] = 0;
+  calibration[2] = 0;
+  calibration[3] = 0;
+  calibration[4] = 0;
+  calibration[5] = 0;
+  calibration[6] = 0;
+  calibration[7] = 0;
+  calibration[8] = 0;
+  calibration[9] = 0;
+  calibration[10] = 0;
+  calibration[11] = 0;
   
 
   delay(2000);
   
   sensor_init();
 
+  delay(1000);
+
+  for(int i = 0; i < 1000; i++){
+
+    sensor_task(&gSensorData);
+    
+    rawSensorData[0] = gSensorData.gyro.x;
+    rawSensorData[1] = gSensorData.gyro.y;
+    rawSensorData[2] = gSensorData.gyro.z;
+    rawSensorData[3] = gSensorData.accel.x;
+    rawSensorData[4] = gSensorData.accel.y;
+    rawSensorData[5] = gSensorData.accel.z;
+  
+    KalmanFilter(state, rawSensorData);
+
+    if(i >= 990){
+      calibration[0] += state[0];
+      calibration[1] += state[1];
+      calibration[2] += state[2];
+      calibration[3] += state[3];
+      calibration[4] += state[4];
+      calibration[5] += state[5];
+      calibration[6] += state[6];
+      calibration[7] += state[7];
+      calibration[8] += state[8];
+      calibration[9] += state[9];
+      calibration[10] += state[10];
+      calibration[11] += state[11];
+    }
+    
+  }
+
+  calibration[0] = calibration[0]/10;
+  calibration[1] = calibration[1]/10;
+  calibration[2] = calibration[2]/10;
+  calibration[3] = calibration[3]/10;
+  calibration[4] = calibration[4]/10;
+  calibration[5] = calibration[5]/10;
+  calibration[6] = calibration[6]/10;
+  calibration[7] = calibration[7]/10;
+  calibration[8] = calibration[8]/10;
+  calibration[9] = calibration[9]/10;
+  calibration[10] = calibration[10]/10;
+  calibration[11] = calibration[11]/10 - 9.81;
+
 }
 
 void loop() {
 
-  //solenoid_open(SOLENOID1);
-  //solenoid_open(SOLENOID2);
-
   sensor_task(&gSensorData);
-
-  //sensor_print(&gSensorData);
 
   rawSensorData[0] = gSensorData.gyro.x;
   rawSensorData[1] = gSensorData.gyro.y;
@@ -56,13 +108,19 @@ void loop() {
 
   KalmanFilter(state, rawSensorData);
 
-  state_print(state);
+  output[0] = state[0] - calibration[0];
+  output[1] = state[1] - calibration[1];
+  output[2] = state[2] - calibration[2];
+  output[3] = state[3] - calibration[3];
+  output[4] = state[4] - calibration[4];
+  output[5] = state[5] - calibration[5];
+  output[6] = state[6] - calibration[6];
+  output[7] = state[7] - calibration[7];
+  output[8] = state[8] - calibration[8];
+  output[9] = state[9] - calibration[9];
+  output[10] = state[10] - calibration[10];
+  output[11] = state[11] - calibration[11];
 
-  //delay(1000);
-
-  //solenoid_close(SOLENOID1);
-  //solenoid_close(SOLENOID2);
-
-  //delay(1000);
+  state_print(output);
 
 }
